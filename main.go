bd76835201e0c206b0c0710978ddb463134431e4
Subhas Bhat <bhatsubhas@gmail.com>
2021-11-16 17:06:47 +0530

    Add code to respond with list of people on invocation

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type Person struct {
+	Name string `json:"name"`
+}
+
+var people = []Person{
+	{"Alex"},
+	{"Bob"},
+}
 
 func main() {
 	lambda.Start(HandleRequest)
 }
 
 func HandleRequest(req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	msg, err := json.Marshal(people)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 500,
+			Body:       "Oops!!something went wrong",
+		}, nil
+	}
 	return events.APIGatewayV2HTTPResponse{
 		StatusCode: 200,
-		Body:       "Sample Response",
+		Body:       string(msg),
 	}, nil
 }
